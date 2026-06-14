72ec6f3a4cc3ddb0b550062bd075af0c645d5485
Dgaray2 <dgaray2@lsu.edu>
2026-06-13 20:03:20 -0500

    connect homepage and testimonies to Sanity CMS

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,7 @@ import { schemaTypes } from './sanity/schemaTypes'
 export default defineConfig({
   name: 'default',
   title: 'TSTMNY',
+  basePath: '/studio',
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
